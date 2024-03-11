3c5ed638ad3dde13a56db2e13fdd26dd976e5f32
rajsinghtech <rajsingh@iastate.edu>
2024-03-10 21:29:06 -0500

    Splitup functions into seperate packages

--- internal/controller/dashboard_controller.go
+++ internal/controller/dashboard_controller.go
@@ -17,20 +17,14 @@ limitations under the License.
 package controller
 
 import (
-	// "fmt"
 	"context"
-	"strings"
-	"reflect"
+
 	homerv1alpha1 "github.com/rajsinghtech/homer-operator.git/api/v1alpha1"
-	"gopkg.in/yaml.v2"
+	homer "github.com/rajsinghtech/homer-operator.git/pkg/homer"
+	networkingv1 "k8s.io/api/networking/v1"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
-	networkingv1 "k8s.io/api/networking/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/util/intstr"
-
-	// "os"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
@@ -57,136 +51,93 @@ type DashboardReconciler struct {
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.17.0/pkg/reconcile
 func (r *DashboardReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	log := log.FromContext(ctx)
+	var objects []client.Object
 	var dashboard homerv1alpha1.Dashboard
-	// Resource Deleted - Clean up all resources
 	if err := r.Get(ctx, req.NamespacedName, &dashboard); err != nil {
 		if client.IgnoreNotFound(err) != nil {
 			log.Error(err, "unable to fetch Dashboard", "dashboard", req.NamespacedName)
 			return ctrl.Result{}, client.IgnoreNotFound(err)
 		}
-		// Delete all deployments with "homer.rajsingh.info/name": dashboard.Name annotation
-		deployments := &appsv1.DeploymentList{}
-		if err := r.List(ctx, deployments, client.MatchingLabels{"homer.rajsingh.info/name": req.NamespacedName.Name}); err != nil {
-			log.Error(err, "unable to list Deployments", "dashboard", req.NamespacedName)
-			return ctrl.Result{}, err
+		labelSelector := client.MatchingLabels{"dashboard.homer.rajsingh.info/name": req.NamespacedName.Name}
+		// List of resources to delete
+		resources := []client.ObjectList{
+			&appsv1.DeploymentList{},
+			&corev1.ServiceList{},
+			&corev1.ConfigMapList{},
 		}
-		for _, deployment := range deployments.Items {
-			if err := r.Delete(ctx, &deployment); err != nil {
-				log.Error(err, "unable to delete Deployment", "dashboard", req.NamespacedName)
+	
+		for _, resource := range resources {
+			if err := r.List(ctx, resource, labelSelector); err != nil {
+				log.Error(err, "unable to list resources", "dashboard", req.NamespacedName)
 				return ctrl.Result{}, err
 			}
-			log.Info("Deployment deleted", "deployment", deployment)
-		}
-		services := &corev1.ServiceList{}
-		if err := r.List(ctx, services, client.MatchingLabels{"homer.rajsingh.info/name": req.NamespacedName.Name}); err != nil {
-			log.Error(err, "unable to list Services", "dashboard", req.NamespacedName)
-			return ctrl.Result{}, err
-		}
-		for _, service := range services.Items {
-			if err := r.Delete(ctx, &service); err != nil {
-				log.Error(err, "unable to delete Service", "dashboard", req.NamespacedName)
-				return ctrl.Result{}, err
+			for _, resource := range resources {
+				switch typedResource := resource.(type) {
+				case *appsv1.DeploymentList:
+					for _, deployment := range typedResource.Items {
+						if err := r.Delete(ctx, &deployment); err != nil {
+							log.Error(err, "unable to delete deployment", "deployment", deployment.Name)
+							return ctrl.Result{}, err
+						}
+						log.Info("Resource deleted", "resource", deployment)
+					}
+				case *corev1.ServiceList:
+					for _, service := range typedResource.Items {
+						if err := r.Delete(ctx, &service); err != nil {
+							log.Error(err, "unable to delete service", "service", service.Name)
+							return ctrl.Result{}, err
+						}
+						log.Info("Resource deleted", "resource", service)
+					}
+				case *corev1.ConfigMapList:
+					for _, configMap := range typedResource.Items {
+						if err := r.Delete(ctx, &configMap); err != nil {
+							log.Error(err, "unable to delete configMap", "configMap", configMap.Name)
+							return ctrl.Result{}, err
+						}
+						log.Info("Resource deleted", "resource", configMap)
+					}
+				default:
+					log.Info("Unknown resource type", "resource", resource)
+				}
 			}
-			log.Info("Service deleted", "service", service)
-		}
-		configMaps := &corev1.ConfigMapList{}
-		if err := r.List(ctx, configMaps, client.MatchingLabels{"homer.rajsingh.info/name": req.NamespacedName.Name}); err != nil {
-			log.Error(err, "unable to list ConfigMaps", "dashboard", req.NamespacedName)
-			return ctrl.Result{}, err
-		}
-		for _, configMap := range configMaps.Items {
-			if err := r.Delete(ctx, &configMap); err != nil {
-				log.Error(err, "unable to delete ConfigMap", "dashboard", req.NamespacedName)
-				return ctrl.Result{}, err
-			}
-			log.Info("ConfigMap deleted", "configMap", configMap)
 		}
 		return ctrl.Result{}, nil
 	}
-	// Get Ingresses
 	ingresses := &networkingv1.IngressList{}
 	if err := r.List(ctx, ingresses); err != nil {
 		log.Error(err, "unable to list Ingresses", "dashboard", req.NamespacedName)
 		return ctrl.Result{}, err
 	}
-	// Generate Dashboard
-	dashboard = updateHomerConfig(dashboard, *ingresses)
-	// Resource Created/Updated - Create/Update resources
-	configMapSpec, err := r.createConfigMap(dashboard)
-	if err != nil {
-		log.Error(err, "unable to create ConfigMap", "dashboard", req.NamespacedName)
-		return ctrl.Result{}, err
+	// Resource Created - Create all resources
+	deployment := homer.CreateDeployment(dashboard.Name, dashboard.Namespace)
+	service := homer.CreateService(dashboard.Name, dashboard.Namespace)
+	configMap := homer.CreateConfigMap(dashboard.Spec.HomerConfig, dashboard.Name, dashboard.Namespace, *ingresses)
+	objects = append(objects, &deployment)
+	objects = append(objects, &service)
+	objects = append(objects, &configMap)
+	for _, object := range objects {
+		err := r.Get(ctx, client.ObjectKey{Namespace: object.GetNamespace(), Name: object.GetName()}, object)
+		switch {
+		case err != nil:
+			err = r.Create(ctx, object)
+			if err != nil {
+				log.Error(err, "unable to create object", "object", object)
+				return ctrl.Result{}, err
+			}
+			log.Info("Resource created", "resource", object)
+		case client.IgnoreNotFound(err) != nil:
+			log.Error(err, "unable to fetch object", "object", object)
+			return ctrl.Result{}, err
+		default:
+			err = r.Update(ctx, object)
+			if err != nil {
+				log.Error(err, "unable to update object", "object", object)
+				return ctrl.Result{}, err
+			}
+			log.Info("Resource updated", "resource", object)
+		}
 	}
-	// Check if the ConfigMap already exists
-	if err := r.Get(ctx, client.ObjectKey{Namespace: configMapSpec.Namespace, Name: configMapSpec.Name}, &corev1.ConfigMap{}); err != nil {
-		if client.IgnoreNotFound(err) != nil {
-			log.Error(err, "unable to fetch ConfigMap", "dashboard", req.NamespacedName)
-			return ctrl.Result{}, client.IgnoreNotFound(err)
-		}
-		// If not found, create the ConfigMap
-		if err := r.Create(ctx, &configMapSpec); err != nil {
-			log.Error(err, "unable to create ConfigMap", "dashboard", req.NamespacedName)
-			return ctrl.Result{}, err
-		}
-		log.Info("ConfigMap created", "ConfigMap", configMapSpec)
-	} else {
-		// If found, update the ConfigMap
-		if err := r.Update(ctx, &configMapSpec); err != nil {
-			log.Error(err, "unable to update ConfigMap", "dashboard", req.NamespacedName)
-			return ctrl.Result{}, err
-		}
-		log.Info("ConfigMap updated", "ConfigMap", configMapSpec)
-	}
-	deploymentSpec, err := r.createDeployment(dashboard)
-	if err != nil {
-		log.Error(err, "unable to create Deployment", "dashboard", req.NamespacedName)
-		return ctrl.Result{}, err
-	}
-	// Check if the Deployment already exists
-	if err := r.Get(ctx, client.ObjectKey{Namespace: deploymentSpec.Namespace, Name: deploymentSpec.Name}, &appsv1.Deployment{}); err != nil {
-		if client.IgnoreNotFound(err) != nil {
-			log.Error(err, "unable to fetch Deployment", "dashboard", req.NamespacedName)
-			return ctrl.Result{}, client.IgnoreNotFound(err)
-		}
-		// If not found, create the Deployment
-		if err := r.Create(ctx, &deploymentSpec); err != nil {
-			log.Error(err, "unable to create Deployment", "dashboard", req.NamespacedName)
-			return ctrl.Result{}, err
-		}
-		log.Info("Deployment created", "deployment", deploymentSpec)
-	} else {
-		// If found, update the Deployment
-		if err := r.Update(ctx, &deploymentSpec); err != nil {
-			log.Error(err, "unable to update Deployment", "dashboard", req.NamespacedName)
-			return ctrl.Result{}, err
-		}
-		log.Info("Deployment updated", "deployment", deploymentSpec)
-	}
-	serviceSpec, err := r.createService(dashboard)
-	if err != nil {
-		log.Error(err, "unable to create Service", "dashboard", req.NamespacedName)
-		return ctrl.Result{}, err
-	}
-	// Check if the Service already exists
-	if err := r.Get(ctx, client.ObjectKey{Namespace: serviceSpec.Namespace, Name: serviceSpec.Name}, &corev1.Service{}); err != nil {
-		if client.IgnoreNotFound(err) != nil {
-			log.Error(err, "unable to fetch Service", "dashboard", req.NamespacedName)
-			return ctrl.Result{}, client.IgnoreNotFound(err)
-		}
-		// If not found, create the Service
-		if err := r.Create(ctx, &serviceSpec); err != nil {
-			log.Error(err, "unable to create Service", "dashboard", req.NamespacedName)
-			return ctrl.Result{}, err
-		}
-		log.Info("Service created", "service", serviceSpec)
-	} else {
-		// If found, update the Service
-		if err := r.Update(ctx, &serviceSpec); err != nil {
-			log.Error(err, "unable to update Service", "dashboard", req.NamespacedName)
-			return ctrl.Result{}, err
-		}
-		log.Info("Service updated", "service", serviceSpec)
-	}	
 	return ctrl.Result{}, nil
 }
 
@@ -195,160 +146,4 @@ func (r *DashboardReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&homerv1alpha1.Dashboard{}).
 		Complete(r)
-}
-
-func updateHomerConfig(dashboard homerv1alpha1.Dashboard, ingresses networkingv1.IngressList) homerv1alpha1.Dashboard {
-	var services []homerv1alpha1.Service = dashboard.Spec.HomerConfig.Services
-	// iterate over all ingresses and add them to the dashboard
-	for _, ingress := range ingresses.Items {
-		for _, rule := range ingress.Spec.Rules {
-			item := homerv1alpha1.Item{}
-			service := homerv1alpha1.Service{}
-			service.Name = ingress.ObjectMeta.Namespace
-			item.Name = ingress.ObjectMeta.Name
-			service.Logo = "https://raw.githubusercontent.com/kubernetes/community/master/icons/png/resources/labeled/ns-128.png"
-			if len(ingress.Spec.TLS) > 0 {
-				item.Url = "https://" + rule.Host
-			} else {
-				item.Url = "http://" + rule.Host
-			}
-			item.Logo = "https://raw.githubusercontent.com/kubernetes/community/master/icons/png/resources/labeled/ing-128.png"
-			item.Subtitle = rule.Host
-			for key, value := range ingress.ObjectMeta.Annotations {
-				if strings.HasPrefix(key, "item.homer.rajsingh.info/"){
-					fieldName := strings.TrimPrefix(key, "item.homer.rajsingh.info/")
-					reflect.ValueOf(&item).Elem().FieldByName(fieldName).SetString(value)
-				}
-				if strings.HasPrefix(key, "service.homer.rajsingh.info/"){
-					fieldName := strings.TrimPrefix(key, "service.homer.rajsingh.info/")
-					reflect.ValueOf(&service).Elem().FieldByName(fieldName).SetString(value)
-				}
-			}
-			service.Items = append(service.Items, item)
-			services = append(services, service)
-		}
-	}
-	for _, s1 := range services {
-		complete := false
-		for j, s2 := range dashboard.Spec.HomerConfig.Services {
-			if s1.Name == s2.Name {
-				dashboard.Spec.HomerConfig.Services[j].Items = append(s2.Items, s1.Items[0])
-				complete = true
-				break
-			}
-		}
-		if !complete {
-			dashboard.Spec.HomerConfig.Services = append(dashboard.Spec.HomerConfig.Services, s1)
-		}
-	}
-	return dashboard
-}
-
-func (r *DashboardReconciler) createConfigMap(dashboard homerv1alpha1.Dashboard) (corev1.ConfigMap, error) {
-	objYAML, err := yaml.Marshal(dashboard.Spec.HomerConfig)
-	if err != nil {
-		return corev1.ConfigMap{}, err
-	}
-
-	cm := &corev1.ConfigMap{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      dashboard.Spec.ConfigMap.Name,
-			Namespace: dashboard.Namespace,
-			Labels: map[string]string{
-				"managed-by":               "homer-operator",
-				"homer.rajsingh.info/name": dashboard.Name,
-			},
-		},
-		Data: map[string]string{
-			"config.yml": string(objYAML),
-		},
-	}
-	return *cm, nil
-}
-
-func (r *DashboardReconciler) createDeployment(dashboard homerv1alpha1.Dashboard) (appsv1.Deployment, error) {
-	var replicas int32 = 1
-	image := "b4bz/homer"
-	d := &appsv1.Deployment{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      dashboard.Name,
-			Namespace: dashboard.Namespace,
-			Labels: map[string]string{
-				"managed-by":               "homer-operator",
-				"homer.rajsingh.info/name": dashboard.Name,
-			},
-		},
-		Spec: appsv1.DeploymentSpec{
-			Replicas: &replicas,
-			Selector: &metav1.LabelSelector{
-				MatchLabels: map[string]string{
-					"homer.rajsingh.info/name": dashboard.Name,
-				},
-			},
-			Template: corev1.PodTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{
-					Labels: map[string]string{
-						"homer.rajsingh.info/name": dashboard.Name,
-					},
-				},
-				Spec: corev1.PodSpec{
-					Containers: []corev1.Container{
-						{
-							Name:  dashboard.Name,
-							Image: image,
-							VolumeMounts: []corev1.VolumeMount{
-								{
-									Name:      "config-volume",
-									MountPath: "/www/assets",
-								},
-							},
-							Ports: []corev1.ContainerPort{
-								{
-									ContainerPort: 8080,
-								},
-							},
-						},
-					},
-					Volumes: []corev1.Volume{
-						{
-							Name: "config-volume",
-							VolumeSource: corev1.VolumeSource{
-								ConfigMap: &corev1.ConfigMapVolumeSource{
-									LocalObjectReference: corev1.LocalObjectReference{
-										Name: dashboard.Spec.ConfigMap.Name,
-									},
-								},
-							},
-						},
-					},
-				},
-			},
-		},
-	}
-
-	return *d, nil
-}
-func (r *DashboardReconciler) createService(dashboard homerv1alpha1.Dashboard) (corev1.Service, error) {
-	s := &corev1.Service{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      dashboard.Name,
-			Namespace: dashboard.Namespace,
-			Labels: map[string]string{
-				"managed-by":               "homer-operator",
-				"homer.rajsingh.info/name": dashboard.Name,
-			},
-		},
-		Spec: corev1.ServiceSpec{
-			Selector: map[string]string{
-				"homer.rajsingh.info/name": dashboard.Name,
-			},
-			Ports: []corev1.ServicePort{
-				{
-					Port:       80,
-					TargetPort: intstr.FromInt(8080),
-				},
-			},
-		},
-	}
-	return *s, nil
 }
---
+++ pkg/homer/config.go
@@ -0,0 +1,232 @@
+package homer
+
+import (
+	"os"
+	"reflect"
+	"strings"
+
+	yaml "gopkg.in/yaml.v2"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	networkingv1 "k8s.io/api/networking/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+)
+
+type HomerConfig struct {
+	Title    string        `json:"title,omitempty"`
+	Subtitle string        `json:"subtitle,omitempty"`
+	Logo     string        `json:"logo,omitempty"`
+	Header   string        `json:"header,omitempty"`
+	Services []Service     `json:"services,omitempty"`
+	Footer   string        `json:"footer,omitempty"`
+	Defaults DefaultConfig `json:"defaults,omitempty"`
+	Links    []Link        `json:"links,omitempty"`
+}
+
+type ProxyConfig struct {
+	UseCredentials bool `json:"useCredentials,omitempty"`
+}
+
+type DefaultConfig struct {
+	Layout     string `json:"layout,omitempty"`
+	ColorTheme string `json:"colorTheme,omitempty"`
+}
+
+type Service struct {
+	Name  string `json:"name,omitempty"`
+	Icon  string `json:"icon,omitempty"`
+	Logo  string `json:"logo,omitempty"`
+	Items []Item `json:"items,omitempty"`
+}
+
+type Item struct {
+	Name       string `json:"name,omitempty"`
+	Logo       string `json:"logo,omitempty"`
+	Subtitle   string `json:"subtitle,omitempty"`
+	Tag        string `json:"tag,omitempty"`
+	Keywords   string `json:"keywords,omitempty"`
+	Url        string `json:"url,omitempty"`
+	Target     string `json:"target,omitempty"`
+	Tagstyle   string `json:"tagstyle,omitempty"`
+	Type       string `json:"type,omitempty"`
+	Class      string `json:"class,omitempty"`
+	Background string `json:"background,omitempty"`
+}
+
+type Link struct {
+	Name   string `json:"name,omitempty"`
+	Icon   string `json:"icon,omitempty"`
+	Url    string `json:"url,omitempty"`
+	Target string `json:"target,omitempty"`
+}
+
+// LoadConfigFromFile loads HomerConfig from a YAML file.
+func LoadConfigFromFile(filename string) (*HomerConfig, error) {
+	config := HomerConfig{}
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
+func CreateConfigMap(config HomerConfig, name string, namespace string, ingresses networkingv1.IngressList) corev1.ConfigMap {
+	config = UpdateHomerConfig(config, ingresses)
+	objYAML, err := yaml.Marshal(config)
+	if err != nil {
+		return corev1.ConfigMap{}
+	}
+	cm := &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+			Labels: map[string]string{
+				"managed-by":               "homer-operator",
+				"dashboard.homer.rajsingh.info/name": name,
+			},
+		},
+		Data: map[string]string{
+			"config.yml": string(objYAML),
+		},
+	}
+	return *cm
+}
+
+func CreateDeployment(name string, namespace string) appsv1.Deployment {
+	var replicas int32 = 1
+	image := "b4bz/homer"
+	d := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+			Labels: map[string]string{
+				"managed-by":               "homer-operator",
+				"dashboard.homer.rajsingh.info/name": name,
+			},
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: &replicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"dashboard.homer.rajsingh.info/name": name,
+				},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"dashboard.homer.rajsingh.info/name": name,
+					},
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  name,
+							Image: image,
+							VolumeMounts: []corev1.VolumeMount{
+								{
+									Name:      "config-volume",
+									MountPath: "/www/assets",
+								},
+							},
+							Ports: []corev1.ContainerPort{
+								{
+									ContainerPort: 8080,
+								},
+							},
+						},
+					},
+					Volumes: []corev1.Volume{
+						{
+							Name: "config-volume",
+							VolumeSource: corev1.VolumeSource{
+								ConfigMap: &corev1.ConfigMapVolumeSource{
+									LocalObjectReference: corev1.LocalObjectReference{
+										Name: name,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	return *d
+}
+
+func CreateService(name string, namespace string) (corev1.Service) {
+	s := &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+			Labels: map[string]string{
+				"managed-by":               "homer-operator",
+				"dashboard.homer.rajsingh.info/name": name,
+			},
+		},
+		Spec: corev1.ServiceSpec{
+			Selector: map[string]string{
+				"dashboard.homer.rajsingh.info/name": name,
+			},
+			Ports: []corev1.ServicePort{
+				{
+					Port:       80,
+					TargetPort: intstr.FromInt(8080),
+				},
+			},
+		},
+	}
+	return *s
+}
+func UpdateHomerConfig(config HomerConfig, ingresses networkingv1.IngressList) HomerConfig {
+	var services []Service
+	// iterate over all ingresses and add them to the dashboard
+	for _, ingress := range ingresses.Items {
+		for _, rule := range ingress.Spec.Rules {
+			item := Item{}
+			service := Service{}
+			service.Name = ingress.ObjectMeta.Namespace
+			item.Name = ingress.ObjectMeta.Name
+			service.Logo = "https://raw.githubusercontent.com/kubernetes/community/master/icons/png/resources/labeled/ns-128.png"
+			if len(ingress.Spec.TLS) > 0 {
+				item.Url = "https://" + rule.Host
+			} else {
+				item.Url = "http://" + rule.Host
+			}
+			item.Logo = "https://raw.githubusercontent.com/kubernetes/community/master/icons/png/resources/labeled/ing-128.png"
+			item.Subtitle = rule.Host
+			for key, value := range ingress.ObjectMeta.Annotations {
+				if strings.HasPrefix(key, "item.homer.rajsingh.info/"){
+					fieldName := strings.TrimPrefix(key, "item.homer.rajsingh.info/")
+					reflect.ValueOf(&item).Elem().FieldByName(fieldName).SetString(value)
+				}
+				if strings.HasPrefix(key, "service.homer.rajsingh.info/"){
+					fieldName := strings.TrimPrefix(key, "service.homer.rajsingh.info/")
+					reflect.ValueOf(&service).Elem().FieldByName(fieldName).SetString(value)
+				}
+			}
+			service.Items = append(service.Items, item)
+			services = append(services, service)
+		}
+	}
+	for _, s1 := range services {
+		complete := false
+		for j, s2 := range config.Services {
+			if s1.Name == s2.Name {
+				config.Services[j].Items = append(s2.Items, s1.Items[0])
+				complete = true
+				break
+			}
+		}
+		if !complete {
+			config.Services = append(config.Services, s1)
+		}
+	}
+	return config
+}
